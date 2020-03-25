68681a0064495e695f28f0baafe207fb71783b10
jeanjack <tessier.paulalexandre@orange.fr>
2020-03-25 23:58:21 +0100

    add tests init cancel

--- test/initCancelOrderTest.js
+++ test/initCancelOrderTest.js
@@ -1,5 +1,8 @@
-const {createOrder, initCancelOrder} = require("./utils/orderMethods");
+const truffleAssert = require('truffle-assertions');
+const {createOrder, validateOrder, initCancelOrder, completeValidationOrder, takeOrder, deliverOrder} = require("./utils/orderMethods");
 const DeliveryContract = artifacts.require("DeliveryContract");
+const {generateKeyHash} = require('./utils/tools');
+const {SELLER_PRICE, DELIVER_PRICE, actors} = require('./utils/constants');
 
 contract("cancel order method of DeliveryContract", accounts => {
 
@@ -15,5 +18,71 @@ contract("cancel order method of DeliveryContract", accounts => {
     it("Buyer can init cancel order ", async () => {
         await createOrder(deliveryInstance, buyer, seller, deliver, buyer);
         await initCancelOrder(deliveryInstance, buyer, buyer);
+    });
+
+    it("Buyer can validate order and init cancel order ", async () => {
+        await createOrder(deliveryInstance, buyer, seller, deliver, buyer);
+        await validateOrder(deliveryInstance,
+            actors.BUYER,
+            buyer,
+            DELIVER_PRICE + SELLER_PRICE,
+            0,
+            false,
+            true,
+            false,
+            false);
+        await initCancelOrder(deliveryInstance, buyer, buyer);
+    });
+
+    it("Seller can init cancel order ", async () => {
+        await createOrder(deliveryInstance, buyer, seller, deliver, buyer);
+        await initCancelOrder(deliveryInstance, seller, buyer);
+    });
+
+    it("Deliver can init cancel order ", async () => {
+        await createOrder(deliveryInstance, buyer, seller, deliver, buyer);
+        await initCancelOrder(deliveryInstance, deliver, buyer);
+    });
+
+    it("Can't change stage when cancel stage", async () => {
+        await createOrder(deliveryInstance, buyer, seller, deliver, buyer);
+        await initCancelOrder(deliveryInstance, deliver, buyer);
+
+        await truffleAssert.reverts(
+            completeValidationOrder(deliveryInstance, buyer, seller, deliver, 0),
+            "The order isn't at the required stage"
+        );
+
+        await truffleAssert.reverts(
+            takeOrder(deliveryInstance, 0, generateKeyHash().key, deliver),
+            "The order isn't at the required stage"
+        );
+
+        await truffleAssert.reverts(
+            deliverOrder(deliveryInstance, seller, deliver, 0, generateKeyHash().key, deliver),
+            "The order isn't at the required stage"
+        );
+    });
+
+    it("Can't init cancel when not in init stage", async () => {
+        await createOrder(deliveryInstance, buyer, seller, deliver, buyer);
+
+        let {keyHashSeller, keyHashBuyer} = await completeValidationOrder(deliveryInstance, buyer, seller, deliver, 0);
+        await truffleAssert.reverts(
+            initCancelOrder(deliveryInstance, deliver, buyer),
+            "The order isn't at the required stage"
+        );
+
+        await takeOrder(deliveryInstance, 0, keyHashSeller.key, deliver);
+        await truffleAssert.reverts(
+            initCancelOrder(deliveryInstance, deliver, buyer),
+            "The order isn't at the required stage"
+        );
+
+        await deliverOrder(deliveryInstance, seller, deliver, 0, keyHashBuyer.key, deliver);
+        await truffleAssert.reverts(
+            initCancelOrder(deliveryInstance, deliver, buyer),
+            "The order isn't at the required stage"
+        );
     });
 });
