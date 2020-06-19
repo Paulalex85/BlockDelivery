d872ff0d892947199e74fc26ef47185fe8a23a01
jeanjack <tessier.paulalexandre@orange.fr>
2020-06-19 18:59:58 +0200

    copy text component updated when copy

--- app/src/components/Main/components/Menu/components/CopyText.tsx
+++ app/src/components/Main/components/Menu/components/CopyText.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {IoMdCopy, IoMdCheckmark} from "react-icons/io";
 import CopyToClipboard from 'react-copy-to-clipboard';
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
 
 type CopyTextProps = {
     value: string;
@@ -10,18 +11,36 @@ const CopyText = ({value}: CopyTextProps) => {
 
     const [copied, setCopied] = useState(false);
     let size = 25;
+    let tooltipText;
+
+    const handleCopy = () => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 3000);
+    };
 
     let logo;
     if (copied) {
-        logo = <IoMdCheckmark size={size} />;
+        tooltipText = "Copied";
+        logo = <IoMdCheckmark size={size}/>;
     } else {
+        tooltipText = "Copy";
         logo = <IoMdCopy size={size}/>;
     }
 
     return (
-        <CopyToClipboard text={value} onCopy={() => setCopied(true)}>
-            {logo}
-        </CopyToClipboard>
+        <OverlayTrigger
+            placement={"bottom"}
+            overlay={
+                <Tooltip id={"copyTooltip"}>
+                    {tooltipText}
+                </Tooltip>
+            }>
+            <CopyToClipboard text={value} onCopy={() => handleCopy()}>
+                <span>
+                    {logo}
+                </span>
+            </CopyToClipboard>
+        </OverlayTrigger>
     )
 };
 
