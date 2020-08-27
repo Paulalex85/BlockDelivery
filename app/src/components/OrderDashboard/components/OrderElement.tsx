1bf1d0dc9bc9b7c68521dd1bb480a58a30f1be22
jeanjack <tessier.paulalexandre@orange.fr>
2020-08-28 01:17:32 +0200

    add order info in dashboard

--- app/src/components/OrderDashboard/components/OrderElement.tsx
+++ app/src/components/OrderDashboard/components/OrderElement.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {createEthersContract} from "../../../utils/createEthersContract";
-import {ListGroup} from 'react-bootstrap';
+import {ListGroup, Col, Collapse, Row, Badge} from 'react-bootstrap';
 import {BsChevronUp, BsChevronDown} from "react-icons/bs";
 
 type Props = {
@@ -33,25 +33,107 @@ const OrderElement = (props: Props) => {
     }, [orderData]);
 
     return (
-        <ListGroup.Item
-            action
-            variant={colorListGroup}
-            className="text-center"
-            onClick={() => setOpen(!open)}
-            aria-controls="collapse-order"
-            aria-expanded={open}>
+        <React.Fragment>
+            {orderData !== undefined ?
+                <Col className="col-sm-6 col-sm-offset-3">
+                    <ListGroup.Item
+                        action
+                        variant={colorListGroup}
+                        className="text-center"
+                        onClick={() => setOpen(!open)}
+                        aria-controls="collapse-order"
+                        aria-expanded={open}>
                     <span className="float-left">
                         Order #{props.orderId}
                     </span>
-            <span>
+                        <span>
                         {title}
                     </span>
-            {open ?
-                <BsChevronUp/>
-                :
-                <BsChevronDown/>
+                        {open ?
+                            <BsChevronUp className="float-right" size='2em'/>
+                            :
+                            <BsChevronDown className="float-right" size='2em'/>
+                        }
+                    </ListGroup.Item>
+                    <Collapse in={open}>
+                        <ListGroup.Item id="collapse-order">
+                            <h5>Users</h5>
+                            <Row className="mb-3">
+                                <Col>
+                                    <Badge className="mr-3" variant="info">Buyer</Badge>
+                                    {orderData.buyer !== undefined ? orderData.buyer : ""}
+                                </Col>
+                            </Row>
+                            <Row className="mb-3">
+                                <Col>
+                                    <Badge className="mr-3" variant="info">Seller</Badge>
+                                    {orderData.seller !== undefined ? orderData.seller : ""}
+                                </Col>
+                            </Row>
+                            <Row className="mb-3">
+                                <Col>
+                                    <Badge className="mr-3" variant="info">Deliver</Badge>
+                                    {orderData.deliver !== undefined ? orderData.deliver : ""}
+                                </Col>
+                            </Row>
+                            <h5>Delay</h5>
+                            <Row className="mb-3">
+                                <Col>
+                                    {/*Creation : {order.date.toLocaleTimeString() + " " + order.date.toLocaleDateString()}*/}
+                                </Col>
+                                <Col>
+                                    {/*Max delivery : {order.dateDelay.toLocaleTimeString() + " " + order.dateDelay.toLocaleDateString()}*/}
+                                </Col>
+                            </Row>
+                            <h5>Price</h5>
+                            <Row className="mb-3">
+                                <Col>
+                                    Seller
+                                    : {orderData.sellerPrice !== undefined ? orderData.sellerPrice.toString() : ""}
+                                </Col>
+                                <Col>
+                                    Deliver
+                                    : {orderData.deliverPrice !== undefined ? orderData.deliverPrice.toString() : ""}
+                                </Col>
+                            </Row>
+                            {/*<h5>Order Validation</h5>*/}
+                            {/*<Row className="mb-3">*/}
+                            {/*    <Col className="sm-1">*/}
+                            {/*        {order.validateBuyer === "1" ?*/}
+                            {/*            <Badge variant="success">Buyer</Badge>*/}
+                            {/*            :*/}
+                            {/*            <Badge variant="danger">Buyer</Badge>*/}
+                            {/*        }*/}
+                            {/*    </Col>*/}
+                            {/*    <Col className="sm-1">*/}
+                            {/*        {order.validateSeller === "1" ?*/}
+                            {/*            <Badge variant="success">Seller</Badge>*/}
+                            {/*            :*/}
+                            {/*            <Badge variant="danger">Seller</Badge>*/}
+                            {/*        }*/}
+                            {/*    </Col>*/}
+                            {/*    <Col className="sm-1">*/}
+                            {/*        {order.validateDeliver === "1" ?*/}
+                            {/*            <Badge variant="success">Deliver</Badge>*/}
+                            {/*            :*/}
+                            {/*            <Badge variant="danger">Deliver</Badge>*/}
+                            {/*        }*/}
+                            {/*    </Col>*/}
+                            {/*</Row>*/}
+                            {/*<h5>Infos</h5>*/}
+                            {/*<Row className="mb-3">*/}
+                            {/*    <Col>*/}
+                            {/*        {order.details}*/}
+                            {/*    </Col>*/}
+                            {/*</Row>*/}
+
+                        </ListGroup.Item>
+                    </Collapse>
+                </Col>
+
+                : <div/>
             }
-        </ListGroup.Item>
+        </React.Fragment>
     )
 };
 
