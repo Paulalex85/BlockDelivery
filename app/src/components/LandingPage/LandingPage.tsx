e526ab7ece7163eb2a967b36d65e82b9a50eb17a
Paulalex85 <tessier.paulalexandre@orange.fr>
2021-09-09 16:44:23 +0200

    update landing page

--- app/src/components/LandingPage/LandingPage.tsx
+++ app/src/components/LandingPage/LandingPage.tsx
@@ -1,31 +1,20 @@
-import React, {Component} from "react";
-import "./css/landing-page.css"
-import {Col, Container, Form, Row} from 'react-bootstrap'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faFileInvoiceDollar, faLock} from '@fortawesome/free-solid-svg-icons'
-import {faGithub} from '@fortawesome/free-brands-svg-icons'
-import {LoginButtonLandingPage} from "./components";
+import React, { Component } from 'react';
+import './css/landing-page.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileInvoiceDollar, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 class LandingPage extends Component {
-
-    render() {
+    render(): JSX.Element {
         return (
             <div>
                 <header className="masthead text-white text-center">
-                    <div className="overlay"/>
+                    <div className="overlay" />
                     <Container>
                         <Row>
                             <Col xl={9} className="mx-auto">
-                                <h1 className="mb-5">
-                                    Use blockchain and decentralization for package deliveries
-                                </h1>
-                            </Col>
-                            <Col md={10} lg={8} xl={7} className="mx-auto">
-                                <Form>
-                                    <Form.Row>
-                                        <LoginButtonLandingPage/>
-                                    </Form.Row>
-                                </Form>
+                                <h1 className="mb-5">Package delivery on Ethereum</h1>
                             </Col>
                         </Row>
                     </Container>
@@ -37,34 +26,33 @@ class LandingPage extends Component {
                             <Col lg={4}>
                                 <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                                     <div className="features-icons-icon d-flex">
-                                        <FontAwesomeIcon icon={faLock}
-                                                         className="m-auto text-primary"/>
+                                        <FontAwesomeIcon icon={faLock} className="m-auto text-primary" />
                                     </div>
                                     <h3>Security</h3>
                                     <p className="lead mb-0">
-                                        Use the power of the blockchain for safe transaction and data privacy !
+                                        Use the power of the blockchain for safer transactions and freedom !
                                     </p>
                                 </div>
                             </Col>
                             <Col lg={4}>
                                 <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                                     <div className="features-icons-icon d-flex">
-                                        <FontAwesomeIcon icon={faFileInvoiceDollar}
-                                                         className="m-auto text-primary"/>
+                                        <FontAwesomeIcon icon={faFileInvoiceDollar} className="m-auto text-primary" />
                                     </div>
                                     <h3>Low fees</h3>
-                                    <p className="lead mb-0">Only 5% fees from delivery cost !</p>
+                                    <p className="lead mb-0">Only 1% delivery fee</p>
                                 </div>
                             </Col>
                             <Col lg={4}>
                                 <div className="features-icons-item mx-auto mb-0 mb-lg-3">
                                     <div className="features-icons-icon d-flex">
-                                        <FontAwesomeIcon icon={faGithub}
-                                                         className="m-auto text-primary"/>
+                                        <FontAwesomeIcon icon={faGithub} className="m-auto text-primary" />
                                     </div>
                                     <h3>Transparency</h3>
-                                    <p className="lead mb-0">Check the code on our <a
-                                        href={"https://github.com/Paulalex85/Block-Delivery"}>Github</a> !</p>
+                                    <p className="lead mb-0">
+                                        Check the code on{' '}
+                                        <a href={'https://github.com/Paulalex85/BlockDelivery'}>Github</a> !
+                                    </p>
                                 </div>
                             </Col>
                         </Row>
@@ -74,52 +62,25 @@ class LandingPage extends Component {
                 <section className="showcase">
                     <Container fluid={true} className="p-0">
                         <Row noGutters={true}>
-                            <Col lg={6} className="order-lg-2 text-white showcase-img background-1"/>
+                            <Col lg={6} className="order-lg-2 text-white showcase-img background-1" />
                             <Col lg={6} className="order-lg-1 my-auto showcase-text">
                                 <h2>For customers</h2>
                                 <p className="lead mb-0">
-                                    Take the control of your order: no more hidden fees,
-                                    choose your deliver worker, keep order information between you and
-                                    deliver worker and seller like order detail or delivery address.
+                                    Take the control of your order: no more hidden cost, choose your deliver worker,
+                                    keep order information between you, deliver worker and seller like order detail or
+                                    delivery address.
                                 </p>
                             </Col>
                         </Row>
                         <Row noGutters={true}>
-                            <Col lg={6} className="text-white showcase-img background-2"/>
+                            <Col lg={6} className="text-white showcase-img background-2" />
                             <Col lg={6} className="my-auto showcase-text">
-                                <h2>For deliver workers</h2>
+                                <h2>For workers</h2>
                                 <p className="lead mb-0">
-                                    Feel the real independence of your company.
-                                    Only deal with your clients not with us.
-                                    Get the same resources and visibility than with big companies !
+                                    No third-parties, work directly with peoples. No payment delays, get paid when the
+                                    delivery is finished. Only 1% delivery fee to support the project. Own your network
+                                    and increase it.
                                 </p>
-                            </Col>
-                        </Row>
-                        <Row noGutters={true}>
-                            <Col lg={6} className="order-lg-2 text-white showcase-img background-3"/>
-                            <Col lg={6} className="order-lg-1 my-auto showcase-text">
-                                <h2>For sellers</h2>
-                                <p className="lead mb-0">
-                                    Sell food or packages without fees.
-                                </p>
-                            </Col>
-                        </Row>
-                    </Container>
-                </section>
-
-                <section className="call-to-action text-white text-center">
-                    <div className="overlay"/>
-                    <Container>
-                        <Row>
-                            <Col xl={9} className="mx-auto">
-                                <h2 className="mb-4">Ready to get started? Start now!</h2>
-                            </Col>
-                            <Col md={10} lg={8} xl={7} className="mx-auto">
-                                <Form>
-                                    <Form.Row>
-                                        <LoginButtonLandingPage/>
-                                    </Form.Row>
-                                </Form>
                             </Col>
                         </Row>
                     </Container>
@@ -131,30 +92,39 @@ class LandingPage extends Component {
                             <Col lg={6} className="h-100 text-center text-lg-left my-auto">
                                 <ul className="list-inline mb-2">
                                     <li className="list-inline-item">
-                                        <a href="/" className="text-dark">About</a>
+                                        <a href="/" className="text-dark">
+                                            About
+                                        </a>
                                     </li>
                                     <li className="list-inline-item">&sdot;</li>
                                     <li className="list-inline-item">
-                                        <a href="/" className="text-dark">Contact</a>
+                                        <a href="/" className="text-dark">
+                                            Contact
+                                        </a>
                                     </li>
                                     <li className="list-inline-item">&sdot;</li>
                                     <li className="list-inline-item">
-                                        <a href="/" className="text-dark">Terms of Use</a>
+                                        <a href="/" className="text-dark">
+                                            Terms of Use
+                                        </a>
                                     </li>
                                     <li className="list-inline-item">&sdot;</li>
                                     <li className="list-inline-item">
-                                        <a href="/" className="text-dark">Privacy Policy</a>
+                                        <a href="/" className="text-dark">
+                                            Privacy Policy
+                                        </a>
                                     </li>
                                 </ul>
-                                <p className="text-muted small mb-4 mb-lg-0">&copy; Your Website 2019. All Rights
-                                    Reserved.</p>
+                                <p className="text-muted small mb-4 mb-lg-0">Block Delivery</p>
                             </Col>
                             <Col lg={6} className="h-100 text-center text-lg-right my-auto">
                                 <ul className="list-inline mb-0">
                                     <li className="list-inline-item mr-3">
                                         <a href="https://github.com/Paulalex85/Block-Delivery">
-                                            <FontAwesomeIcon icon={faGithub}
-                                                             className="fab fa-2x fa-fw m-auto text-dark"/>
+                                            <FontAwesomeIcon
+                                                icon={faGithub}
+                                                className="fab fa-2x fa-fw m-auto text-dark"
+                                            />
                                         </a>
                                     </li>
                                 </ul>
